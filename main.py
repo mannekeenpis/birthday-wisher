073047c373c90c1e723a5303483fdee16fdca594
Lenar Gasimov <lv4262122+github@gmail.com>
2021-08-11 14:41:02 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = ""
+MY_PASSWORD = ""
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthdays_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) s letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthdays_person["name"])
+
+    with smtplib.SMTP()
